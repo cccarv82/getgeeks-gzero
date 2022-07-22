14e80bb0db59ca457ac7a968441037c31ebd53dd
Carlos Carvalho <c.b.carvalho@accenture.com>
2022-07-22 18:18:59 -0300

    robocop fix

--- backend/run.sh
+++ backend/run.sh
@@ -1,10 +1,13 @@
 rm -rf report/*
 robotidy -c AlignSettingsSection:up_to_column=3:min_width=50 -c AlignTestCasesSection:enabled=True:widths=14,24 --line-length 80 -c AlignKeywordsSection:widths=10,10,24,30 --line-length 100 -c AlignVariablesSection:min_width=20 -c IndentNestedKeywords:enabled=True -c OrderTags:enabled=True tests
-robocop --report all --output ./report/robocop.txt tests
 #executar tudo
 #robot -x xunit.xml -d ./report tests/
 #robot -x xunit.xml -d ./report -i temp tests/Geeks.robot
 pabot --processes 5 -x xunit.xml -d ./report tests/
+
+# Relatório de Code Review
+touch report/robocop.txt
+robocop --report all --output report/robocop.txt tests
 
 # Criação do dashboard com métricas
 # https://github.com/adiralashiva8/robotframework-metrics
--- frontend/run.sh
+++ frontend/run.sh
@@ -1,6 +1,5 @@
 rm -rf report/*
 robotidy -c AlignSettingsSection:up_to_column=3:min_width=50 -c AlignTestCasesSection:enabled=True:widths=14,24 --line-length 80 -c AlignKeywordsSection:widths=10,10,24,30 --line-length 100 -c AlignVariablesSection:min_width=20 -c IndentNestedKeywords:enabled=True -c OrderTags:enabled=True tests/
-robocop --report all --output ./report/robocop.txt tests
 robot -l NONE -o NONE -r NONE tasks/DatabasePreparation.robot
 #executar tudo
 #robot -d ./report -v BROWSER:chromium -v HEADLESS:False tests/
@@ -21,6 +20,10 @@ mkdir report/browser/screenshot
 cp $(find report/pabot_results -type f -name "*.png") report/browser/screenshot/
 rm -rf report/pabot_results/
 rm -rf .pabotsuitenames
+
+# Relatório de Code Review
+touch report/robocop.txt
+robocop --report all --output report/robocop.txt tests
 
 # Criação do dashboard com métricas
 # https://github.com/adiralashiva8/robotframework-metrics
